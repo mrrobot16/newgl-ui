f462a19f6240fb0bd22de5c0b5e4e9b7748ee544
Hector Garcia <hmgarcia06@gmail.com>
2026-06-21 20:33:47 -0400

    Changed BASE_API_URL with Fly.io urls

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -2,10 +2,10 @@ export type AppEnv = "local" | "development" | "staging" | "production";
 export const APP_ENV = process.env.APP_ENV as AppEnv;
 export const NEXT_PUBLIC_APP_ENV = process.env.NEXT_PUBLIC_APP_ENV as AppEnv;
 
-export const LOCAL_BASE_API_URL = "http://localhost:3001/api";
-export const DEV_BASE_API_URL = "http://localhost:3001/api";
-export const STAGING_BASE_API_URL = "https://api-staging.example.com/api";
-export const PRODUCTION_BASE_API_URL = "https://api.example.com/api";
+export const LOCAL_BASE_API_URL = "http://localhost:8080/api";
+export const DEV_BASE_API_URL = "http://localhost:8080/api";
+export const STAGING_BASE_API_URL = "https://newgl-api.fly.dev/api";
+export const PRODUCTION_BASE_API_URL = "https://newgl-api.fly.dev/api";
 
 export const APP_ENV_CONFIG: Record<AppEnv, { BASE_API_URL: string }> = {
   local: { BASE_API_URL: LOCAL_BASE_API_URL },
